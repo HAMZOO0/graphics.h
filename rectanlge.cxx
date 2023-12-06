aa43971f1bf1ed7bcc250127d47eedc6ffc15b9b
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-12-05 23:53:17 -0800

    Create rectanlge.cxx

---
+++ rectanlge.cxx
@@ -0,0 +1,19 @@
+#include<graphics.h>
+#include<conio.h>
+
+int main()
+{
+	// window open (width (pixel ), height , window name)
+  initwindow(1000,1000,"Snake ");
+  rectangle(100,20,200,300); // x1,y1 - x2,y2 
+/*
+      x1,y1 *
+      
+      
+      
+      
+      
+                        * x2,y2
+*/
+   getch();
+}
