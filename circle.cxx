f494d79aac7113753e40c90f703ce2c6fd7129fd
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-12-05 23:44:31 -0800

    Create circle.cxx

---
+++ circle.cxx
@@ -0,0 +1,13 @@
+#include<graphics.h>
+#include<conio.h>
+
+int main()
+{
+	// window open (width (pixel ), height , window name)
+  initwindow(1000,1000,"Snake ");
+  
+  circle(100,200,60);// 60 --> coordinate 
+  circle(400,200,60);// 60 --> coordinate 
+
+   getch();
+}
