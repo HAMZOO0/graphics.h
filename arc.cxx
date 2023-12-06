57e511e1abaa11cee0cca3ed2b2536360c2090b8
Hamza Khan <98114762+HAMZOO0@users.noreply.github.com>
2023-12-06 03:25:30 -0800

    Create arc.cxx

---
+++ arc.cxx
@@ -0,0 +1,14 @@
+#include<graphics.h>
+#include<conio.h>
+
+int main()
+{
+	// window open (width (pixel ), height , window name)
+  initwindow(1000,1000,"Snake ");
+
+arc(200,200,90,180,100); // arc (x , y , starting angle (90 ) , end angle (180) , radius/size/(100 pix))
+
+
+
+   getch();
+}
